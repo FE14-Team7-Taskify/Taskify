2315c9f99d000dce1aaaaa652c1468b6d7927ae1
callu9 <callu_9ine@naver.com>
2025-04-30 10:11:19 +0900

    TAS-25: 임시 추가한 OverlayProvider 삭제

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,16 @@
 import { AuthProvider } from '@/contexts/AuthProvider';
-import { OverlayProvider } from '@/contexts/OverlayProvider';
 import '@/styles/globals.scss';
-import { MutationCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient({
-    mutationCache: new MutationCache({
-      onError: (error) => {
-        console.log(error.message);
-      },
-    }),
-  });
+  const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <OverlayProvider>
-        <AuthProvider>
-          <Component {...pageProps} />
-        </AuthProvider>
-      </OverlayProvider>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
     </QueryClientProvider>
   );
 }
