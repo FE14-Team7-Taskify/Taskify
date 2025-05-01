2267956d844710bce15dde75c11db6364d8a8b65
callu9 <callu_9ine@naver.com>
2025-05-02 05:31:02 +0900

    TAS-47: 모달용 버튼 스타일 적용 TAS-46: ModalWrapper 활용한 TwoButtonModal 컴포넌트 추가

--- src/components/modal/ModalWrapper.tsx
+++ src/components/modal/ModalWrapper.tsx
@@ -1,11 +1,5 @@
 import { HTMLAttributes } from 'react';
 import styles from './modal.module.scss';
-import Button from '../common/button/loginButton/LoginButton';
-
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  text?: string;
-  onClick?: () => void;
-}
 
 interface OverlayProps extends HTMLAttributes<HTMLDivElement> {
   title?: string;
