2867c17f69a4a630346479fb7efd32da9a0a81c0
callu9 <callu_9ine@naver.com>
2025-05-01 14:51:44 +0900

    TAS-49: Pagination 버튼 공통 컴포넌트 생성

---
+++ src/components/common/button/Pagination.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import styles from './pagination.module.scss';
+
+interface PaginationProps {
+  totalPage?: number;
+  currentPage?: number;
+  setPage: (page: number) => void;
+}
+
+export default function Pagination({ totalPage = 1, currentPage = 1, setPage }: PaginationProps) {
+  return (
+    <div className={styles.paginationBtns} id="pagination-btns">
+      <button
+        className={styles.btnLeft}
+        disabled={currentPage === 1}
+        onClick={() => setPage(currentPage - 1)}
+      >
+        <Image src="/icon/arrow_right.svg" alt="이전 페이지 아이콘" width={16} height={16} />
+      </button>
+      <button
+        className={styles.btnRight}
+        disabled={currentPage === totalPage}
+        onClick={() => setPage(currentPage + 1)}
+      >
+        <Image src="/icon/arrow_right.svg" alt="다음 페이지 아이콘" width={16} height={16} />
+      </button>
+    </div>
+  );
+}
