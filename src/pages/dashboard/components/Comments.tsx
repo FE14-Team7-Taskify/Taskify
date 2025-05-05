0bab4a1b16f8b0f099a42b838953e1e5e71e874e
yujin0408 <aszx5849@naver.com>
2025-05-05 16:05:18 +0900

    TAS-57: 댓글 입력, 삭제 기능 추가

--- src/pages/dashboard/components/Comments.tsx
+++ src/pages/dashboard/components/Comments.tsx
@@ -1,10 +1,15 @@
 import { cn } from '@/styles/util/stylesUtil';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/comments.module.scss';
-import { useCommentsQuery } from '@/api/comments/comments.query';
+import {
+  useCommentsQuery,
+  useCreateCommentMutation,
+  useDeleteCommentMutation,
+} from '@/api/comments/comments.query';
 import { CommentType } from '@/api/comments/comments.schema';
 import Image from 'next/image';
 import { useUser } from '@/contexts/AuthProvider';
+import { useQueryClient } from '@tanstack/react-query';
 
 interface comments {
   cardId: number;
@@ -19,10 +24,21 @@ interface comment {
 function Comment({ comment }: comment) {
   const author = comment.author;
   const user = useUser();
+  const commentDeleteMutation = useDeleteCommentMutation(comment.id);
+  const queryClient = useQueryClient();
   const pad = (n: number) => String(n).padStart(2, '0');
-
   const date = new Date(comment.createdAt);
   const dateFormat = `${date.getUTCFullYear()}.${pad(date.getUTCMonth() + 1)}.${pad(date.getUTCDate())} ${pad(date.getUTCHours())}:${pad(date.getUTCMinutes())}`;
+
+  const handleDeleteComment = () => {
+    commentDeleteMutation.mutate(comment.id, {
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: ['comments', comment.cardId],
+        });
+      },
+    });
+  };
 
   return (
     <div className={cn(styles.commentWrap)}>
@@ -34,8 +50,10 @@ function Comment({ comment }: comment) {
       <p className={cn(styles.comment)}>{comment.content}</p>
       {user?.id === author.id && (
         <div className={cn(styles.commentControls)}>
-          <button>수정</button>
-          <button>삭제</button>
+          <button type="button">수정</button>
+          <button type="button" onClick={handleDeleteComment}>
+            삭제
+          </button>
         </div>
       )}
     </div>
@@ -44,13 +62,43 @@ function Comment({ comment }: comment) {
 
 function Comments({ cardId, columnId, dashboardId }: comments) {
   const { data } = useCommentsQuery({ cardId });
+  const INITIAL_VALUES = {
+    content: '',
+    cardId,
+    columnId,
+    dashboardId,
+  };
+  const [values, setValues] = useState(INITIAL_VALUES);
+  const createCommentMutation = useCreateCommentMutation(cardId);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = e.target;
+
+    setValues((prev) => ({
+      ...prev,
+      content: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    createCommentMutation.mutate(values);
+    setValues(INITIAL_VALUES);
+  };
 
   return (
     <div className={cn(styles.commentArea)}>
       <p>댓글</p>
-      <form className={cn(styles.commentForm)}>
-        <textarea className={cn(styles.commentInput)} placeholder="댓글 작성하기"></textarea>
-        <button type="submit">입력</button>
+      <form className={cn(styles.commentForm)} onSubmit={handleSubmit}>
+        <textarea
+          className={cn(styles.commentInput)}
+          placeholder="댓글 작성하기"
+          onChange={handleInputChange}
+          value={values.content}
+        ></textarea>
+        <button type="submit" disabled={!values.content.trim()}>
+          입력
+        </button>
       </form>
       <div className={cn(styles.commentsList)}>
         {data?.comments.map((comment) => <Comment key={comment.id} comment={comment} />)}
