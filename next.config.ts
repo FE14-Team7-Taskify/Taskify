117d8425293356d197a6d4f9b739fc4c9ebc8e3f
yujin0408 <108394961+yujin0408@users.noreply.github.com>
2025-05-05 10:00:47 +0900

    TAS-56, TAS-58 : 할 일 카드 UI 구현 및 카드 삭제 기능 추가
    * TAS-56: 할 일 카드 UI 구현
    
    * TAS-56: 할 일 카드 반응형UI 추가
    
    * TAS-58 : 카드 삭제 기능 추가

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig: import('next').NextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
+  },
 };
 
 export default nextConfig;
