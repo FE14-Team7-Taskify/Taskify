0ea4775fa3b76f98b59673768de5625b98115754
HyunseokLEE <unducklee@gmail.com>
2025-05-06 11:46:56 +0900

    TAS: 83: 기능 1,2 section UI 완료

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,8 @@ export default function HomePage() {
 
   return (
     <div className={styles.container}>
-      {/* 히어로 섹션 */}
-      <section className={styles.hero}>
+      {/* 메인 섹션 */}
+      <section className={styles.main}>
         <Image src={src} alt="Illustration" width={width} height={height} />
         <h1>
           새로운 일정 관리 <span className={styles.highlight}>Taskify</span>
@@ -20,32 +20,40 @@ export default function HomePage() {
       </section>
 
       {/* 기능 1 */}
-      <section className={styles.feature}>
-        <div className={styles.textBox}>
+      <section className={styles.point}>
+        <div className={styles.title}>
           <p>Point 1</p>
-          <h2>
-            일의 우선순위를
-            <br />
-            관리하세요
-          </h2>
         </div>
-        <div className={styles.imageBox}>
-          <Image src="/feature-priority.png" alt="우선순위 기능" width={600} height={400} />
+        <div className={styles.textbox}>
+          <div>일의 우선순위를</div>
+          <div>관리하세요</div>
+        </div>
+        <div className={styles.imageBox1}>
+          <Image
+            src="/images/landing/resource/landing1.svg"
+            alt="우선순위"
+            width={296}
+            height={248}
+          />
         </div>
       </section>
 
       {/* 기능 2 */}
-      <section className={styles.featureReverse}>
-        <div className={styles.imageBox}>
-          <Image src="/feature-input.png" alt="일 등록 기능" width={600} height={400} />
-        </div>
-        <div className={styles.textBox}>
+      <section className={`${styles.point} ${styles.secondPoint}`}>
+        <div className={styles.title}>
           <p>Point 2</p>
-          <h2>
-            해야 할 일을
-            <br />
-            등록하세요
-          </h2>
+        </div>
+        <div className={styles.textbox}>
+          <div>해야 할 일을</div>
+          <div>등록하세요</div>
+        </div>
+        <div className={styles.imageBox2}>
+          <Image
+            src="/images/landing/resource/landing2.svg"
+            alt="해야할일"
+            width={217}
+            height={250}
+          />
         </div>
       </section>
 
@@ -54,15 +62,30 @@ export default function HomePage() {
         <h3>생산성을 높이는 다양한 설정 ✨</h3>
         <div className={styles.settingCards}>
           <div className={styles.card}>
-            <Image src="/theme.png" alt="테마 설정" width={300} height={200} />
+            <Image
+              src="/images/landing/resource/landing3.svg"
+              alt="테마 설정"
+              width={300}
+              height={200}
+            />
             <p>다크모드 지원</p>
           </div>
           <div className={styles.card}>
-            <Image src="/category.png" alt="카테고리 설정" width={300} height={200} />
+            <Image
+              src="/images/landing/resource/landing4.svg"
+              alt="카테고리 설정"
+              width={300}
+              height={200}
+            />
             <p>유연한 분류</p>
           </div>
           <div className={styles.card}>
-            <Image src="/notification.png" alt="알림 설정" width={300} height={200} />
+            <Image
+              src="/images/landing/resource/landing5.svg"
+              alt="알림 설정"
+              width={300}
+              height={200}
+            />
             <p>알림으로 일정 체크</p>
           </div>
         </div>
