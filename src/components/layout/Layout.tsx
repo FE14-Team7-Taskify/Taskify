2a8d19d286c6f35c04dc152bcd50f465ef378427
Jiwon42 <93248260+Jiwon42@users.noreply.github.com>
2025-05-06 10:53:25 +0900

    TAS-63: Landing페이지 상단 네비게이션바 및 basic header구현
    TAS-63: Landing페이지 상단 네비게이션바 및 (/mydashboard 부분) basic header구현

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,13 +1,13 @@
 import { useUser } from '@/contexts/AuthProvider';
 import Header from '../header/Header';
 import SideBar from '../sidebar/SideBar';
-
+import styles from './layout.module.scss';
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   const user = useUser();
   return user ? (
     <SideBar>
       <Header />
-      {children}
+      <div className={styles.pageContent}>{children}</div>
     </SideBar>
   ) : (
     children
