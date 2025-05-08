50d90fade949220b2fb51d299c271798b0945c61
HyunseokLEE <unducklee@gmail.com>
2025-05-08 14:01:39 +0900

    feature: 404 페이지 추가

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,17 @@
 // pages/404.tsx
-import Link from "next/link";
+import Link from 'next/link';
+import styles from './404.module.scss';
 
 export default function Custom404() {
   return (
-    <div>
-      <h1>404</h1>
-      <p>페이지를 찾을 수 없습니다.</p>
-      <Link href="/">홈으로 돌아가기</Link>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>404</h1>
+        <p className={styles.message}>페이지를 찾을 수 없습니다.</p>
+        <Link href="/" className={styles.button}>
+          홈으로 돌아가기
+        </Link>
+      </div>
     </div>
   );
 }
